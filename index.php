0a9ea62d04037de3ade725d9419bf3b303492034
AugustoValentin <55113880+AugustoValentin@users.noreply.github.com>
2019-09-10 19:26:58 -0300

    Add files via upload
    MY HTML AND CSS STUDY WITH A WEBSITE!

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+        <title>Primeira page PHP</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+      <?php
+        echo "<a href='saida.php'> Exemplo de Saídas com php (echo vs print)</a>";
+        echo'<br>";//pula uma linha
+        echo "<a href='variaveis.php'> Exemplo de Saídas com php (echo vs print)</a>";
+        echo'<br>";//pula uma linha
+        echo "<a href='palavras.php'> Exemplo de Saídas com php (echo vs print)</a>";
+        echo'<br>";//pula uma linha
+        echo "<a href='matematica.php'> Exemplo de Saídas com php (echo vs print)</a>";
+        echo'<br>";//pula uma linha
+        echo "<a href='global_local.php'> Exemplo de Saídas com php (echo vs print)</a>";
+        echo'<br>";//pula uma linha
+    ?>
+
+    </body>
+</html>
